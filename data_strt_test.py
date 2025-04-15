af30a8deab4acb0df3ca232aabc8cec2ecb394ca
mosesnzioka <mosesnzioka1@gmail.com>
2025-04-15 14:57:44 +0300

    find and print the index of value 30

--- data_strt_test.py
+++ data_strt_test.py
@@ -13,3 +13,6 @@ my_list.extend([50, 60, 70])
 my_list.pop()
 # Sort my_list in ascending order
 my_list.sort()
+# Find and print the index of the value 30 in my_list
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
