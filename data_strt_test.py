03d68a3ac5503e99cb057f44627ac1ea95b74cde
mosesnzioka <mosesnzioka1@gmail.com>
2025-04-15 14:49:29 +0300

    extend my_list with another list

--- data_strt_test.py
+++ data_strt_test.py
@@ -5,5 +5,7 @@ my_list.append(10)
 my_list.append(20)
 my_list.append(30)
 my_list.append(40)
-# Insert the value 15 at the second position (index 1)
+# Insert the value 15 at the second position
 my_list.insert(1, 15)
+# Extend my_list with another list: [50, 60, 70]
+my_list.extend([50, 60, 70])
